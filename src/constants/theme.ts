9c8082cc040f0ab9c48207b6dd3028fe5b24276c
AykutSarac <aykutsarac0@gmail.com>
2022-02-02 00:08:47 +0300

    add new color

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -5,6 +5,7 @@ export const darkTheme: DefaultTheme = {
   FULL_WHITE: "#FFFFFF",
   BLACK: "#202225",
   BLACK_DARK: "#2C2F33",
+  BLACK_LIGHT: "#2F3136",
   BLACK_PRIMARY: "#36393f",
   BLACK_SECONDARY: "#23272A",
   CRIMSON: "#DC143C",
