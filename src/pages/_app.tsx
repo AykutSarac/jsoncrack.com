71b01564ef58080e3503d871a6998bb550d59c8b
AykutSarac <aykutsarac0@gmail.com>
2022-02-02 18:48:22 +0300

    remove layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,12 @@ import GlobalStyle from "src/constants/globalStyle";
 import { darkTheme } from "src/constants/theme";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
-import { Layout } from "src/components/Layout";
 
 function AykutSarac({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={darkTheme}>
-      <Layout>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </Layout>
+      <GlobalStyle />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
