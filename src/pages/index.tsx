09fe688dde444ae5c9762f535378a1a515eda428
AykutSarac <aykutsarac0@gmail.com>
2022-02-02 18:49:56 +0300

    update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Navbar } from "src/components/Navbar";
+import styled from "styled-components";
+
+const StyledHome = styled.div`
+  padding: 24px;
+`;
 
 const Home: React.FC = () => {
-  return <div></div>;
+  return (
+    <StyledHome>
+      <Navbar />
+    </StyledHome>
+  );
 };
 
 export default Home;
