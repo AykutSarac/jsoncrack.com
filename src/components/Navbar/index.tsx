90016aec033b1dced9881dba40f911d8ac2b821e
AykutSarac <aykutsarac0@gmail.com>
2022-02-03 00:03:46 +0300

    style navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ const StyledNavbar = styled.div`
   display: flex;
   justify-content: space-between;
   width: 75%;
-  margin: 0 auto 20px;
+  margin: 0 auto 80px;
 `;
 
 const StyledLogo = styled.div`
