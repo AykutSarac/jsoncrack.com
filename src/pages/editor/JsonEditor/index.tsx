f3b9bf446e6e79358128e4fa866e85e4e93a3583
AykutSarac <aykutsarac0@gmail.com>
2022-02-03 00:14:09 +0300

    update default value

--- src/pages/editor/JsonEditor/index.tsx
+++ src/pages/editor/JsonEditor/index.tsx
@@ -24,23 +24,36 @@ const StyledJSONInput = styled(JSONInput)`
 
 export const defaultValue = [
   {
-    author: "John Doe",
-    pseudonym: "J.D.",
-    books: [
+    Author: "J. K. Rowling.",
+    Genre: "Fantasy",
+    Characters: ["Hermione Granger", "Harry Potter", "Lord Voldemort", "MORE"],
+    Books: [
+      { title: "Philosopher's Stone", date: "1997" },
       {
-        title: "Book 1",
+        title: "Chamber of Secrets",
+        date: "1998",
       },
       {
-        title: "Book 2",
-        chapters: [
-          {
-            title: "No Way Home",
-            page: 256,
-          },
-        ],
+        title: "Prisoner of Azkaban",
+        date: "1999",
+      },
+      {
+        title: "Goblet of Fire",
+        date: "1999",
+      },
+      {
+        title: "Order of the Phoenix",
+        date: "2003",
+      },
+      {
+        title: "Half-Blood Prince",
+        date: "2005",
+      },
+      {
+        title: "Deathly Hallows",
+        date: "2007",
       },
     ],
-    age: 26,
   },
 ];
 
