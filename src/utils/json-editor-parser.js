a1ec883bd5f820240f0ef82c99eb0eedd11d5cd8
AykutSarac <aykutsarac0@gmail.com>
2022-02-03 14:36:56 +0300

    disable reset localStorage on error

--- src/utils/json-editor-parser.js
+++ src/utils/json-editor-parser.js
@@ -59,7 +59,6 @@ export const parser = (input) => {
     return [...flatten(res), ...relationships(res)];
   } catch (error) {
     console.error("An error occured while parsin JSON data!", error.stack);
-    localStorage.setItem('json', JSON.stringify(defaultValue));
     return Array;
   }
 };
