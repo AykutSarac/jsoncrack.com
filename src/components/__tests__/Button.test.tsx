ed09c71d925b613850046549a6427c4ce1184aea
AykutSarac <aykutsarac0@gmail.com>
2022-02-03 22:06:58 +0300

    setup test environment

---
+++ src/components/__tests__/Button.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "src/components/Button";
+import { screen, render } from '@testing-library/react';
+
+describe("Button", () => {
+  it("should render Button component", () => {
+    render(<Button>Click Me!</Button>);
+    expect(screen.getByRole('button', { name: /Click Me/ })).toBeInTheDocument();
+  });
+});
