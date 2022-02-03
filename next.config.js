91bd509cb234734fd32677cbd51990ea52533fed
AykutSarac <aykutsarac0@gmail.com>
2022-02-03 21:05:23 +0300

    prepare config for deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 const isProd = process.env.NODE_ENV === "production";
-const assetPrefix = isProd ? '/jsonvisio.com' : ''
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/jsonvisio.com',
   exportPathMap: () => ({
     '/': { page: '/' },
     '/editor': { page: '/editor' },
   }),
-  assetPrefix,
   reactStrictMode: true,
   trailingSlash: true,
 };
