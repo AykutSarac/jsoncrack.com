eec999bf0a3c85a8cea4fa68e7962b857258af91
AykutSarac <aykutsarac0@gmail.com>
2022-02-03 21:06:25 +0300

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,6 +38,29 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta name="title" content="JSON Visio" />
+          <meta
+            name="description"
+            content="Simple visualization tool for your JSON data. No forced structure, paste your JSON data and view it instantly."
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://jsonvisio.com/" />
+          <meta property="og:title" content="JSON Visio" />
+          <meta
+            property="og:description"
+            content="Simple visualization tool for your JSON data. No forced structure, paste your JSON data and view it instantly."
+          />
+          <meta property="og:image" content="https://jsonvisio.com/image.png" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://jsonvisio.com/" />
+          <meta property="twitter:title" content="JSON Visio" />
+          <meta
+            property="twitter:description"
+            content="Simple visualization tool for your JSON data. No forced structure, paste your JSON data and view it instantly."
+          />
+          <meta property="twitter:image" content="https://jsonvisio.com/image.png" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
