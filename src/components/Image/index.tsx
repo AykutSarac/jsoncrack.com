395eba5af6c5a6f48088ed437ef8fc485f216d51
AykutSarac <aykutsarac0@gmail.com>
2022-04-06 15:01:46 +0300

    lint

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledImage = styled.img`
   object-fit: contain;
   height: auto;
-  
+
   @media only screen and (max-width: 768px) {
     width: 100%;
   }
 `;
 
 export const Image = ({ ...props }) => {
-  return <StyledImage {...props} />
+  return <StyledImage {...props} />;
 };
