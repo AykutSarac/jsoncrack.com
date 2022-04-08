b6fc8d7ef49740a4d42511f71c94767f1abd3d69
AykutSarac <aykutsarac0@gmail.com>
2022-04-08 16:57:21 +0300

    implement HOC context

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import { Editor } from "src/containers/Editor";
+import { WithConfig } from "src/hocs/config";
 
 const EditorPage: React.FC = () => {
   return (
@@ -8,7 +9,9 @@ const EditorPage: React.FC = () => {
       <Head>
         <title>Editor | JSON Visio</title>
       </Head>
-      <Editor />
+      <WithConfig>
+        <Editor />
+      </WithConfig>
     </>
   );
 };
