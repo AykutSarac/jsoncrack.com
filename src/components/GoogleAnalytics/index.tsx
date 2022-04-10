8c940d7e135470f2a9e659cf61c086194cb8c6c1
AykutSarac <aykutsarac0@gmail.com>
2022-04-10 16:01:34 +0300

    seperate scripts

---
+++ src/components/GoogleAnalytics/index.tsx
@@ -0,0 +1,28 @@
+import Script from "next/script";
+import React from "react";
+import * as gtag from "src/utils/gtag";
+
+export const GoogleAnalytics: React.FC = () => {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+};
