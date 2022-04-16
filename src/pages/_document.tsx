b81f9cc281223bc835b267482c7e272aee62601b
AykutSarac <aykutsarac0@gmail.com>
2022-04-16 12:41:06 +0300

    fix styled-components FOUC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,38 @@ import Document, {
   Head,
   Main,
   NextScript,
+  DocumentContext,
+  DocumentInitialProps,
 } from "next/document";
 import { SeoTags } from "src/components/SeoTags";
+import { ServerStyleSheet } from "styled-components";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 
 class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, <>{sheet.getStyleElement()}</>],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html lang="en">
