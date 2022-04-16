1cf090805b02b9d3a393da696d18836f12cfb1cf
AykutSarac <aykutsarac0@gmail.com>
2022-04-16 12:36:58 +0300

    update config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
 const withTM = require("next-transpile-modules")(["reaflow"]);
 
 const nextConfig = withTM({
+  reactStrictMode: true,
   exportPathMap: () => ({
     "/": { page: "/" },
     "/editor": { page: "/editor" },
   }),
   experimental: {
     esmExternals: "loose",
+    outputStandalone: true,
   },
   compiler: {
     styledComponents: true,
     swcMinify: true,
+    removeConsole: {
+      exclude: ["error"],
+    },
   },
 });
 
