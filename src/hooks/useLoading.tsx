0aca219661fc42fba2a2787038cb5deeecaea70b
AykutSarac <aykutsarac0@gmail.com>
2022-04-27 17:32:03 +0300

    remove unused hook

--- src/hooks/useLoading.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-export const useLoading = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => setLoading(false), []);
-
-  return loading;
-};
