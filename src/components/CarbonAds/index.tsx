2e83dcb27e14aad3ff0a7cae20b7b635af26b10d
AykutSarac <aykutsarac0@gmail.com>
2022-06-14 11:42:51 +0300

    fix ads script

--- src/components/CarbonAds/index.tsx
+++ src/components/CarbonAds/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
-const isDevelopment = process.env.NODE_ENV === "development";
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 export const CarbonAds = () => {
-  if (isDevelopment) return null;
-  
   return (
-    <>
+    <StyledWrapper>
       <script
-        async
         type="text/javascript"
         src="//cdn.carbonads.com/carbon.js?serve=CE7IPKQL&placement=jsonvisiocom"
         id="_carbonads_js"
       ></script>
-    </>
+    </StyledWrapper>
   );
 };
