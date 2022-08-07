8e7835c21f4a0e206ac5948efa4c466a02f5e25a
AykutSarac <aykutsarac0@gmail.com>
2022-08-07 10:16:40 +0300

    style sponsors tooltip color

--- src/components/Sponsors/index.tsx
+++ src/components/Sponsors/index.tsx
@@ -50,6 +50,7 @@ const StyledSponsor = styled.li<{ handle: string }>`
       border-radius: 4px;
       font-weight: 500;
       font-size: 14px;
+      color: ${({ theme }) => theme.TEXT_NORMAL};
     }
 
     &::after {
