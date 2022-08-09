f81f7df64edd62e474b180bd9cc4b43a138c0594
AykutSarac <aykutsarac0@gmail.com>
2022-08-09 16:40:02 +0300

    update config

--- next.config.js
+++ next.config.js
@@ -6,15 +6,11 @@ const withPWA = require("next-pwa");
 const nextConfig = {
   reactStrictMode: true,
   exportPathMap: () => ({
-    "/": { page: "/" },
-    "/editor": { page: "/Editor" },
-    "/widget": { page: "/Widget" },
+    "/": { page: "/Home" },
+    "/editor": { page: "/Editor", query: { title: "json" } },
+    "/widget": { page: "/Widget", query: { title: "json" } },
   }),
-  compiler: {
-    styledComponents: true,
-  },
   pwa: {
-    // disable: process.env.NODE_ENV === "development",
     disable: true, // disable temp until issue #61 solved
     dest: "public",
     fallbacks: {
