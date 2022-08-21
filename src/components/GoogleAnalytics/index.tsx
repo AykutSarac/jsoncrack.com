a9d0b45bdce962dd39470b6123c4b600ced1606e
AykutSarac <aykutsarac0@gmail.com>
2022-08-21 15:46:08 +0300

    update gtag script

--- src/components/GoogleAnalytics/index.tsx
+++ src/components/GoogleAnalytics/index.tsx
@@ -9,24 +9,18 @@ export const GoogleAnalytics: React.FC = () => {
   return (
     <>
       <Script
-        id="google-tag-manager"
+        src="https://www.googletagmanager.com/gtag/js?id=G-JKZEHMJBMH"
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-JKZEHMJBMH`}
       />
-      <Script
-        id="gtag-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-JKZEHMJBMH', {
-                page_path: window.location.pathname,
-            });
-        `,
-        }}
-      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-JKZEHMJBMH');
+        `}
+      </Script>
     </>
   );
 };
