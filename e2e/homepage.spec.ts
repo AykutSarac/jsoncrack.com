62a99935b3ed8297beef82b33bf8df45409186e5
depapp <depa.article@gmail.com>
2022-09-02 13:02:29 +0700

    test: add more e2e test

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -1,19 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('homepage test', async ({ page }) => {
-
-  // open homepage
+test.beforeEach(async ({ page }) => {
+  // open home page
   await page.goto('/');
-  //verify homepage title
+  // verify home page title
   await expect(page).toHaveTitle('JSON Crack - Crack your data into pieces');
+});
 
+test('home page test', async ({ page }) => {
   // open JSON Crack goals modals
   const buttonGoals = page.locator('button:has-text("Help JSON Crack\'s Goals")');
   await buttonGoals.click();
   // verify modals title
-  expect(page.locator('h2:has-text("Help JSON Crack\'s Goals")')).toBeVisible();
+  const modalsTitle = page.locator('h2:has-text("Help JSON Crack\'s Goals")');
+  expect(modalsTitle).toBeVisible();
   // close JSON Crack goals modals
   const buttonClose = page.locator('button:has-text("Close")');
   await buttonClose.click();
-
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -40,6 +40,7 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
