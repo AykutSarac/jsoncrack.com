bf0aa7793f8a0e6a5c6ff5374254e8e6e666dc99
AykutSarac <aykutsarac0@gmail.com>
2022-09-11 13:47:23 +0300

    styling

--- src/components/ErrorContainer/index.tsx
+++ src/components/ErrorContainer/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import {
-  MdExpandMore,
-  MdExpandLess,
   MdReportGmailerrorred,
   MdOutlineCheckCircleOutline,
 } from "react-icons/md";
--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -32,7 +32,6 @@ const StyledToggle = styled.div<{ active: boolean }>`
   padding: 2px;
   border-radius: 14px;
   background: ${({ active }) => (active ? "#3AA55D" : "#72767c")};
-  transition: 0.1s;
   cursor: pointer;
 
   input {
