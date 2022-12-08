bec3679b7ff9f840953ef1cf647dba6ff49746f6
Muhammad Faizan Uddin <faizan.uddin94@gmail.com>
2022-12-08 09:40:38 +0400

    [273] Display trimmed strings when text too long

--- src/components/CustomNode/ObjectNode.tsx
+++ src/components/CustomNode/ObjectNode.tsx
@@ -11,6 +11,12 @@ const ObjectNode: React.FC<CustomNodeProps> = ({ node, x, y }) => {
   const ref = React.useRef(null);
   const performanceMode = useConfig(state => state.performanceMode);
   // const { inViewport } = useInViewport(ref);
+  const getNodeValue = useCallback(value => {
+    if (typeof value === "string" && value.length > 50) {
+      return `${value.substring(0, 10)} ... ${value.substring(value.length - 10, value.length)}`;
+    }
+    return value;
+  }, []);
 
   if (data.isEmpty) return null;
 
@@ -34,7 +40,7 @@ const ObjectNode: React.FC<CustomNodeProps> = ({ node, x, y }) => {
             <Styled.StyledKey objectKey>
               {JSON.stringify(val[0]).replaceAll('"', "")}:{" "}
             </Styled.StyledKey>
-            <Styled.StyledLinkItUrl>{JSON.stringify(val[1])}</Styled.StyledLinkItUrl>
+            <Styled.StyledLinkItUrl>getNodeValue(JSON.stringify(val[1]))}</Styled.StyledLinkItUrl>
           </Styled.StyledRow>
         ))}
     </Styled.StyledForeignObject>
