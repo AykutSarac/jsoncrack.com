abf0c0856ff34361585067a75ec88c18f6c8592e
AykutSarac <aykutsarac0@gmail.com>
2022-12-23 13:36:32 +0300

    create embed docs

--- src/pages/embed.tsx
+++ src/pages/embed.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import dynamic from "next/dynamic";
 import Head from "next/head";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import styled from "styled-components";
+
+const SyntaxHighlighter = dynamic(
+  () => import("react-syntax-highlighter").then(c => c.PrismAsync),
+  {
+    ssr: false,
+  }
+);
 
 const StyledFrame = styled.iframe`
   border: none;
@@ -13,23 +22,34 @@ const StyledPage = styled.div`
   padding: 5%;
 `;
 
-const StyledContent = styled.div`
-  display: flex;
-  gap: 20px;
+const StyledContent = styled.section`
+  margin-top: 20px;
+  background: rgba(181, 116, 214, 0.23);
+  padding: 16px;
+  border-radius: 6px;
 `;
 
 const StyledContentBody = styled.div`
   display: flex;
-  line-height: 2;
-  flex: 1;
+  flex-wrap: wrap;
+  gap: 15px;
+  line-height: 1.8;
+
+  p {
+    flex: 1;
+  }
 `;
 
-const StyledHighlight = styled.span<{ link?: boolean }>`
-  color: ${({ theme, link }) => (link ? theme.BLURPLE : theme.TEXT_POSITIVE)};
+const StyledHighlight = styled.span<{ link?: boolean; alert?: boolean }>`
+  text-align: left;
+  color: ${({ theme, link, alert }) =>
+    alert ? theme.DANGER : link ? theme.BLURPLE : theme.TEXT_POSITIVE};
   background: ${({ theme }) => theme.BACKGROUND_TERTIARY};
   border-radius: 4px;
   font-weight: 500;
   padding: 4px;
+  font-size: 14px;
+  margin: ${({ alert }) => alert && "8px 0"};
 `;
 
 const Embed = () => {
@@ -40,8 +60,8 @@ const Embed = () => {
         <meta name="description" content="Embedding JSON Crack tutorial into your websites." />
       </Head>
       <h1>Embed</h1>
-      <h2># Fetching from URL</h2>
       <StyledContent>
+        <h2># Fetching from URL</h2>
         <StyledContentBody>
           <p>
             By adding <StyledHighlight>?json=https://catfact.ninja/fact</StyledHighlight> query at
@@ -56,40 +76,42 @@ const Embed = () => {
               https://jsoncrack.com/editor?json=https://catfact.ninja/fact
             </StyledHighlight>
           </p>
-        </StyledContentBody>
 
-        <StyledFrame
-          scrolling="no"
-          title="Untitled"
-          src="https://codepen.io/AykutSarac/embed/KKBpWVR?default-tab=html%2Cresult"
-          loading="lazy"
-        >
-          See the Pen <a href="https://codepen.io/AykutSarac/pen/KKBpWVR">Untitled</a> by Aykut
-          Saraç (<a href="https://codepen.io/AykutSarac">@AykutSarac</a>) on{" "}
-          <a href="https://codepen.io">CodePen</a>.
-        </StyledFrame>
+          <StyledFrame
+            scrolling="no"
+            title="Untitled"
+            src="https://codepen.io/AykutSarac/embed/KKBpWVR?default-tab=html%2Cresult"
+            loading="eager"
+          >
+            See the Pen <a href="https://codepen.io/AykutSarac/pen/KKBpWVR">Untitled</a> by Aykut
+            Saraç (<a href="https://codepen.io/AykutSarac">@AykutSarac</a>) on{" "}
+            <a href="https://codepen.io">CodePen</a>.
+          </StyledFrame>
+        </StyledContentBody>
       </StyledContent>
-      <h2># Share Saved JSON</h2>
       <StyledContent>
+        <h2># Embed Saved JSON</h2>
         <StyledContentBody>
-          <p>Just like fetching from URL above, you can embed saved public json by adding the json id as <StyledHighlight>?json=....</StyledHighlight>
+          <p>
+            Just like fetching from URL above, you can embed saved public json by adding the json id
+            to &quot;json&quot; query{" "}
+            <StyledHighlight>?json=639b65c5a82efc29a24b2de2</StyledHighlight>
           </p>
+          <StyledFrame
+            scrolling="no"
+            title="Untitled"
+            src="https://codepen.io/AykutSarac/embed/vYaORgM?default-tab=html%2Cresult"
+            loading="lazy"
+          >
+            See the Pen <a href="https://codepen.io/AykutSarac/pen/vYaORgM">Untitled</a> by Aykut
+            Saraç (<a href="https://codepen.io/AykutSarac">@AykutSarac</a>) on{" "}
+            <a href="https://codepen.io">CodePen</a>.
+          </StyledFrame>
         </StyledContentBody>
-
-        <StyledFrame
-          scrolling="no"
-          title="Untitled"
-          src="https://codepen.io/AykutSarac/embed/KKBpWVR?default-tab=html%2Cresult"
-          loading="lazy"
-        >
-          See the Pen <a href="https://codepen.io/AykutSarac/pen/KKBpWVR">Untitled</a> by Aykut
-          Saraç (<a href="https://codepen.io/AykutSarac">@AykutSarac</a>) on{" "}
-          <a href="https://codepen.io">CodePen</a>.
-        </StyledFrame>
       </StyledContent>
-      <h2># Communicating with API</h2>
-      <h3>&gt; Post Message to Embed</h3>
       <StyledContent>
+        <h2># Communicating with API</h2>
+        <h3>◼︎ Post Message to Embed</h3>
         <StyledContentBody>
           <p>
             Communicating with the embed is possible with{" "}
@@ -101,46 +123,50 @@ const Embed = () => {
               MessagePort
             </StyledHighlight>
             , you should pass an object consist of &quot;json&quot; and &quot;options&quot; key
-            where json is string and options is an object that can contain:{" "}
-            <StyledHighlight>
-              theme (&quot;light&quot;, &quot;dark&quot;), direction (&quot;DOWN&quot;,
-              &quot;TOP&quot;, &quot;RIGHT&quot;, &quot;LEFT&quot;)
-            </StyledHighlight>.
+            where json is a string and options is an object that may contain the following:
+            <SyntaxHighlighter language="markdown" style={materialDark} showLineNumbers={true}>
+              {`{\n\ttheme: "light" | "dark",\n\tdirection: "TOP" | "RIGHT" | "DOWN" | "LEFT"\n}`}
+            </SyntaxHighlighter>
           </p>
-        </StyledContentBody>
 
-        <StyledFrame
-          scrolling="no"
-          title="Untitled"
-          src="https://codepen.io/AykutSarac/embed/rNrVyWP?default-tab=html%2Cresult"
-          loading="lazy"
-        >
-          See the Pen <a href="https://codepen.io/AykutSarac/pen/rNrVyWP">Untitled</a> by Aykut
-          Saraç (<a href="https://codepen.io/AykutSarac">@AykutSarac</a>) on{" "}
-          <a href="https://codepen.io">CodePen</a>.
-        </StyledFrame>
+          <StyledFrame
+            scrolling="no"
+            title="Untitled"
+            src="https://codepen.io/AykutSarac/embed/rNrVyWP?default-tab=html%2Cresult"
+            loading="lazy"
+          >
+            See the Pen <a href="https://codepen.io/AykutSarac/pen/rNrVyWP">Untitled</a> by Aykut
+            Saraç (<a href="https://codepen.io/AykutSarac">@AykutSarac</a>) on{" "}
+            <a href="https://codepen.io">CodePen</a>.
+          </StyledFrame>
+        </StyledContentBody>
       </StyledContent>
-      <h3>&gt; On Page Load</h3>
       <StyledContent>
+        <h3>◼︎ On Page Load</h3>
         <StyledContentBody>
           <p>
+            <StyledHighlight as="div" alert>
+              ⚠️ <b>Important!</b> - iframe should be defined before the script tag
+            </StyledHighlight>
+            <StyledHighlight as="div" alert>
+              ⚠️ <b>Note</b> - postMessage should be delayed using setTimeout
+            </StyledHighlight>
             To display JSON on load event, you should post json into iframe using it&apos;s onload
             event like in the example. Make sure to use{" "}
             <StyledHighlight>setTimeout</StyledHighlight> when loading data and set a time around
             500ms otherwise it won&apos;t work.
           </p>
+          <StyledFrame
+            scrolling="no"
+            title="Untitled"
+            src="https://codepen.io/AykutSarac/embed/QWBbpqx?default-tab=html%2Cresult"
+            loading="lazy"
+          >
+            See the Pen <a href="https://codepen.io/AykutSarac/pen/QWBbpqx">Untitled</a> by Aykut
+            Saraç (<a href="https://codepen.io/AykutSarac">@AykutSarac</a>) on{" "}
+            <a href="https://codepen.io">CodePen</a>.
+          </StyledFrame>
         </StyledContentBody>
-
-        <StyledFrame
-          scrolling="no"
-          title="Untitled"
-          src="https://codepen.io/AykutSarac/embed/QWBbpqx?default-tab=html%2Cresult"
-          loading="lazy"
-        >
-          See the Pen <a href="https://codepen.io/AykutSarac/pen/QWBbpqx">Untitled</a> by Aykut
-          Saraç (<a href="https://codepen.io/AykutSarac">@AykutSarac</a>) on{" "}
-          <a href="https://codepen.io">CodePen</a>.
-        </StyledFrame>
       </StyledContent>
     </StyledPage>
   );
