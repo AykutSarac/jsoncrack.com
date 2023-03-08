0126cd37e8c150bc55e94ca120ef975c44234ab2
Aykut Saraç <aykutsarac0@gmail.com>
2023-03-08 12:10:09 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+const withBundleAnalyzer =
+  process.env.ANALYZE === "true"
+    ? require("@next/bundle-analyzer")({
+        enabled: process.env.ANALYZE === "true",
+      })()
+    : x => x;
 
 /**
  * @type {import('next').NextConfig}
