3b4baf7a4d1935161b74934a95ff327065f2a328
AykutSarac <aykutsarac0@gmail.com>
2023-06-18 12:37:46 +0300

    fix: remove console.log

--- src/utils/json/jsonParser.ts
+++ src/utils/json/jsonParser.ts
@@ -74,7 +74,6 @@ export function parser(jsonStr: string): Graph {
       path: getNodePath(states.graph.nodes, states.graph.edges, node.id),
     }));
 
-    console.log(states.graph);
     return states.graph;
   } catch (error) {
     console.error(error);
