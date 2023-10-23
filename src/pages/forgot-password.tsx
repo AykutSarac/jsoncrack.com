f4d5348a60dc4e96448e2002a7efecacdff9d7d0
AykutSarac <aykutsarac0@gmail.com>
2023-10-23 19:36:44 +0300

    Revert "fix reset password"
    This reverts commit 4949526d3fe712dc50985bcd17b14cc527457584.

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Button, Group, Paper, Stack, TextInput, Text, Anchor, PasswordInput } from "@mantine/core";
 import { toast } from "react-hot-toast";
 import Layout from "src/layout/Layout";
 import { supabase } from "src/lib/api/supabase";
-import useUser from "src/store/useUser";
 
 function ResetPassword() {
   const [loading, setLoading] = React.useState(false);
@@ -68,10 +68,11 @@ function ResetPassword() {
 }
 
 const ForgotPassword = () => {
-  const isAuthenticated = useUser(state => state.isAuthenticated);
+  const { query } = useRouter();
   const [loading, setLoading] = React.useState(false);
   const [email, setEmail] = React.useState("");
   const [success, setSuccess] = React.useState(false);
+  const isPasswordReset = query?.type === "recovery" && !query?.error;
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     try {
@@ -96,7 +97,7 @@ const ForgotPassword = () => {
         <title>Reset Password - JSON Crack</title>
         <meta name="robots" content="noindex,nofollow" />
       </Head>
-      {isAuthenticated ? (
+      {isPasswordReset ? (
         <ResetPassword />
       ) : (
         <Paper mx="auto" mt={70} maw={400} p="lg" withBorder>
