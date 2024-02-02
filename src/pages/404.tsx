94ea5ff7199e69ce24a9ec4f1f4b01a1c6373df1
AykutSarac <aykutsarac0@gmail.com>
2024-02-02 10:47:11 +0300

    fix 404 & error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,54 +1,30 @@
 import React from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { Button, Text, Title } from "@mantine/core";
-import styled from "styled-components";
+import Link from "next/link";
+import { Button, Stack, Text, Title } from "@mantine/core";
 import Layout from "src/layout/Layout";
 
-const StyledNotFound = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  margin-top: 0 40px;
-  text-align: center;
-`;
-
-const StyledMessage = styled.h4`
-  color: ${({ theme }) => theme.FULL_WHITE};
-  font-size: 25px;
-  font-weight: 800;
-  margin: 10px 0;
-`;
-
-const StyledSubMessage = styled.div`
-  width: 50%;
-  color: ${({ theme }) => theme.SILVER};
-  margin-bottom: 25px;
-`;
-
-const StyledImageWrapper = styled.div`
-  width: 300px;
-`;
-
 const NotFound: React.FC = () => {
-  const router = useRouter();
-
   return (
     <Layout>
       <Head>
         <meta name="robots" content="noindex,nofollow" />
       </Head>
-      <StyledNotFound>
-        <StyledImageWrapper>
-          <img src="/assets/404.svg" alt="not found" width={300} height={400} />
-        </StyledImageWrapper>
-        <Title c="dark">WIZARDS BEHIND CURTAINS?</Title>
-        <Text c="dark">Looks like you&apos;re lost, let&apos;s head back to the home!</Text>
-        <Button mt="lg" size="lg" type="button" onClick={() => router.push("/")}>
-          Go Home
-        </Button>
-      </StyledNotFound>
+      <Stack mt={100} justify="center" align="center">
+        <Title fz={150} style={{ fontFamily: "monospace" }}>
+          404
+        </Title>
+        <Title order={2}>Nothing to see here</Title>
+        <Text c="dimmed" maw={800} style={{ textAlign: "center" }}>
+          Page you are trying to open does not exist. You may have mistyped the address, or the page
+          has been moved to another URL. If you think this is an error contact support.
+        </Text>
+        <Link href="/">
+          <Button size="lg" color="gray" type="button">
+            Go Home
+          </Button>
+        </Link>
+      </Stack>
     </Layout>
   );
 };
