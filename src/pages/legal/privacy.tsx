435a32aa56a823949aae5dac5b6dc1ef4cb77623
AykutSarac <aykutsarac0@gmail.com>
2024-04-23 20:41:41 +0300

    fix: legal text color

--- src/pages/legal/privacy.tsx
+++ src/pages/legal/privacy.tsx
@@ -11,47 +11,47 @@ const Privacy = () => {
       </Head>
       <Container my={50} size="sm" pb="lg">
         <Paper bg="transparent">
-          <Title c="gray.5">Privacy Policy</Title>
+          <Title c="gray.8">Privacy Policy</Title>
           <Stack my="lg">
-            <Text c="gray.5">
+            <Text c="gray.8">
               This Privacy Policy describes how your personal information is collected, used, and
               shared when you visit or make a purchase from jsoncrack.com (the “Site”).
             </Text>
-            <Title order={3} c="gray.5">
+            <Title order={3} c="gray.8">
               To Whom Does This Policy Apply
             </Title>
-            <Text c="gray.5">
+            <Text c="gray.8">
               This Privacy Policy applies to customers and site visitors. Each customer is
               responsible for posting its own terms, conditions, and privacy policies, and ensuring
               compliance with all applicable laws and regulations.
             </Text>
-            <Title order={3} c="gray.5">
+            <Title order={3} c="gray.8">
               Changes To This Privacy Policy
             </Title>
-            <Text c="gray.5">
+            <Text c="gray.8">
               This Privacy Policy may change from time to time, as our Platform and our business may
               change. Your continued use of the Platform after any changes to this Privacy Policy
               indicates your agreement with the terms of the revised Privacy Policy.
             </Text>
-            <Title order={3} c="gray.5">
+            <Title order={3} c="gray.8">
               What Information Do We Collect
             </Title>
-            <Text c="gray.5">
+            <Text c="gray.8">
               We collect information directly from you when you provide it to us explicitly on our
               Site. We do not use third-party cookies on our Site.
             </Text>
-            <Title order={3} c="gray.5">
+            <Title order={3} c="gray.8">
               What We Use Your Information For
             </Title>
-            <Text c="gray.5">
+            <Text c="gray.8">
               We use your information to provide our Services, to improve our Platform, to
               understand how you use our Platform, and to communicate with you. We DO NOT share any
               personal information to third parties.
             </Text>
-            <Title order={3} c="gray.5">
+            <Title order={3} c="gray.8">
               How To Contact Us
             </Title>
-            <Text c="gray.5">
+            <Text c="gray.8">
               For privacy-related questions, please contact us at{" "}
               <Anchor href="mailto:contact@jsoncrack.com">contact@jsoncrack.com</Anchor>.
             </Text>
--- src/pages/legal/subscription-refund.tsx
+++ src/pages/legal/subscription-refund.tsx
@@ -11,27 +11,27 @@ const SubscriptionRefund = () => {
       </Head>
       <Container my={50} size="sm" pb="lg">
         <Paper bg="transparent">
-          <Title c="gray.5">Subscription & Refund</Title>
+          <Title c="gray.8">Subscription & Refund</Title>
           <Stack my="lg">
-            <Text c="gray.5">
+            <Text c="gray.8">
               This document delineates the Subscription Cancellation and Refund Policy for users of
               jsoncrack.com (the “Site”). It provides guidance on the cancellation process. For
               inquiries or assistance related to cancellations, users are encouraged to contact the
               customer support team at{" "}
               <Anchor href="mailto:contact@jsoncrack.com">contact@jsoncrack.com</Anchor>.
             </Text>
-            <Title order={3} c="gray.5">
+            <Title order={3} c="gray.8">
               Cancellation Policy
             </Title>
-            <Text c="gray.5">
+            <Text c="gray.8">
               You have the right to cancel your subscription at any time. When you cancel, your
               subscription will remain active until the end of the current billing period. You will
               not be billed for any subsequent periods.
             </Text>
-            <Title order={4} c="gray.5">
+            <Title order={4} c="gray.8">
               How to Cancel:
             </Title>
-            <Text c="gray.5">
+            <Text c="gray.8">
               To cancel your subscription, follow these steps:
               <List type="ordered" my="lg">
                 <List.Item>Log in to your account.</List.Item>
@@ -42,20 +42,20 @@ const SubscriptionRefund = () => {
               not eligible for a refund. However, you will still have access to the service until
               the end of the current billing period.
             </Text>
-            <Title order={3} c="gray.5">
+            <Title order={3} c="gray.8">
               Refund Policy
             </Title>
-            <Text c="gray.5">
+            <Text c="gray.8">
               If you cancel your subscription within 3 days of the initial purchase, you are
               eligible for a full refund. Refunds will be issued to the original payment method used
               during the purchase. For refund inquiries or assistance, please contact our customer
               support team at{" "}
               <Anchor href="mailto:contact@jsoncrack.com">contact@jsoncrack.com</Anchor>.
             </Text>
-            <Title order={3} c="gray.5">
+            <Title order={3} c="gray.8">
               Changes to this Policy
             </Title>
-            <Text c="gray.5">
+            <Text c="gray.8">
               We reserve the right to modify this subscription cancellation and refund policy at any
               time. Any changes will be effective immediately upon posting the updated policy on our
               website. It is your responsibility to review this policy periodically for changes. By
