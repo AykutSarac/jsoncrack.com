78d75e04d323bebd70c49631c86acc474726b34a
acpaquette <acp263@nau.edu>
2024-11-14 00:15:51 -0700

    Final pass on the traversal algorithm

--- src/containers/Editor/components/views/GraphView/lib/utils/traverse.ts
+++ src/containers/Editor/components/views/GraphView/lib/utils/traverse.ts
@@ -21,26 +21,26 @@ type Traverse = {
 const traverseArray = (states: States, array: Node, parentId: string) => {
   const graph = states.graph;
   if (array.children) {
+    const parentNode = graph.nodes.at(Number(parentId) - 1);
+    if (parentNode) {
+      parentNode.data.childrenCount = array.children.length;
+    }
     for (let i = 0; i < array.children.length; i++) {
-      if (array.children[i].type === "array") {
-        const nodeId = addNodeToGraph({
-          graph,
-          text: String(i),
-          type: array.children[i].type,
-        });
-        addEdgeToGraph(graph, parentId, nodeId);
+      const child = array.children[i];
+      let nodeText = "";
+      if (child.value !== undefined) {
+        nodeText = String(child.value);
+      }
+      const nodeId = addNodeToGraph({
+        graph,
+        text: String(nodeText),
+        type: child.type,
+      });
+      addEdgeToGraph(graph, parentId, nodeId);
+      if (child.type === "array") {
         traverseArray(states, array.children[i], nodeId);
-      } else if (array.children[i].type === "object") {
-        traverse({ objectToTraverse: array.children[i], states, myParentId: parentId });
-      } else {
-        console.log(array.children[i].value);
-        console.log(array.children[i].type);
-        const nodeId = addNodeToGraph({
-          graph,
-          text: String(array.children[i].value),
-          type: array.children[i].type,
-        });
-        addEdgeToGraph(graph, parentId, nodeId);
+      } else if (child.type === "object") {
+        traverse({ objectToTraverse: child, states, myParentId: nodeId });
       }
     }
   }
@@ -66,37 +66,36 @@ export const traverse = ({ objectToTraverse, states, myParentId }: Traverse) => 
     }
   }
 
-  if (nodeText.length !== 0) {
-    nodeId = addNodeToGraph({ graph, text: nodeText, type });
-  }
-  else if (myParentId) {
-    nodeId = myParentId;
+
+  if (myParentId && nodeText.length !== 0) {
+    nodeId = addNodeToGraph({ graph, text: nodeText });
+    addEdgeToGraph(graph, myParentId, nodeId);
   }
 
-  if (myParentId && nodeId !== myParentId) {
-    addEdgeToGraph(graph, myParentId, nodeId);
+  if (myParentId) {
+    const node = graph.nodes.at(Number(myParentId) - 1);
+    if (node) {
+      node.data.childrenCount = childQueue.length;
+      if (nodeText.length !== 0) {
+        node.data.childrenCount += 1;
+      }
+    }
   }
 
   childQueue.forEach(child => {
     if (child.children) {
+      const brotherNodeId = addNodeToGraph({ 
+        graph, 
+        text: child.children[0].value, 
+        type: child.children[1].type,
+      });
+      if (myParentId) {
+        addEdgeToGraph(graph, myParentId, brotherNodeId);
+      }
       if (child.children[1].type === "object") {
-        // const subNodeId = addNodeToGraph({
-        //   graph,
-        //   text: child.children[0].value,
-        //   type: child.children[1].type,
-        // });
-        // addEdgeToGraph(graph, nodeId, subNodeId);
-        traverse({ objectToTraverse: child.children[1], states, myParentId: myParentId });
+        traverse({ objectToTraverse: child.children[1], states, myParentId: brotherNodeId });
       } else if (child.children[1].type === "array") {
-        const subNodeId = addNodeToGraph({ 
-          graph, 
-          text: child.children[0].value, 
-          type: child.children[1].type,
-        });
-        addEdgeToGraph(graph, nodeId, subNodeId);
-        traverseArray(states, child.children[1], subNodeId);
-      } else {
-        console.log(":(");
+        traverseArray(states, child.children[1], brotherNodeId);
       }
     }
   });
