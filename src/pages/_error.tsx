4662e21a3da7af8fbd7aa83d8ced628e3241413b
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-11-17 22:46:20 +0900

    chore: update _error.tsx (#427)
    Occured -> Occurred

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -10,7 +10,7 @@ const Custom500 = () => {
 
   return (
     <Layout>
-      <NextSeo {...SEO} title="Unexpected Error Occured | ToDiagram" />
+      <NextSeo {...SEO} title="Unexpected Error Occurred | ToDiagram" />
       <Stack mt={100} justify="center" align="center">
         <Title fz={150} style={{ fontFamily: "monospace" }}>
           500
