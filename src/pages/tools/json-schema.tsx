61cb1b1da7bc257ab5d869ca015e5b2687c59682
AykutSarac <aykutsarac0@gmail.com>
2024-12-08 21:04:17 +0300

    feat: add seo tags to json schema tool page

--- src/pages/tools/json-schema.tsx
+++ src/pages/tools/json-schema.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Box, Button, Container, Flex, Paper, Title, Text } from "@mantine/core";
 import { Editor, type OnMount } from "@monaco-editor/react";
 import { JSONSchemaFaker } from "json-schema-faker";
+import { NextSeo } from "next-seo";
 import { LuCheck, LuXCircle } from "react-icons/lu";
+import { SEO } from "src/constants/seo";
 import { FileFormat, TypeLanguage } from "src/enums/file.enum";
 import { editorOptions } from "src/layout/ConverterLayout/options";
 import Layout from "src/layout/PageLayout";
@@ -46,6 +48,12 @@ const JSONSchemaTool = () => {
 
   return (
     <Layout>
+      <NextSeo
+        {...SEO}
+        title="JSON Schema Validator & Generator"
+        description="Use our JSON Schema Validator & Generator tool to easily validate and generate JSON schemas, and generate data from JSON schemas. Simply input your JSON data, generate the corresponding schema, and validate your data with ease."
+        canonical="https://jsoncrack.com/tools/json-schema"
+      />
       <Container mt="xl" size="xl">
         <Title c="black">JSON Schema Validator & Generator</Title>
         <Flex pt="lg" gap="lg">
