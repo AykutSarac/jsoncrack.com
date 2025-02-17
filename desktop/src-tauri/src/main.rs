5bb122994beab18aeb03545639ae66ab8a0b819e
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-02-17 12:50:00 +0530

    chore: remove unused assets

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -3,11 +3,8 @@
     windows_subsystem = "windows"
 )]
 
-use std::time::{SystemTime, UNIX_EPOCH};
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![on_button_clicked])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
