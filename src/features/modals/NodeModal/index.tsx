71b6b74faeab61821895bc94c8141fccf306f9ec
Jeevan Munnangi <116681701+ramfam101@users.noreply.github.com>
2025-07-21 22:27:21 -0400

    update

--- src/features/modals/NodeModal/index.tsx
+++ src/features/modals/NodeModal/index.tsx
@@ -1,51 +1,32 @@
 import React from "react";
 import type { ModalProps } from "@mantine/core";
-import { Modal, Stack, Text, ScrollArea, Flex, CloseButton } from "@mantine/core";
+import { Modal, Stack, Text, ScrollArea } from "@mantine/core";
 import { CodeHighlight } from "@mantine/code-highlight";
-import type { NodeData } from "../../../types/graph";
 import useGraph from "../../editor/views/GraphView/stores/useGraph";
 
-// return object from json removing array and object fields
-const normalizeNodeData = (nodeRows: NodeData["text"]) => {
-  if (!nodeRows || nodeRows.length === 0) return "{}";
-  if (nodeRows.length === 1 && !nodeRows[0].key) return `${nodeRows[0].value}`;
+const dataToString = (data: any) => {
+  const text = Array.isArray(data) ? Object.fromEntries(data) : data;
+  const replacer = (_: string, v: string) => {
+    if (typeof v === "string") return v.replaceAll('"', "");
+    return v;
+  };
 
-  const obj = {};
-  nodeRows?.forEach(row => {
-    if (row.type !== "array" && row.type !== "object") {
-      if (row.key) obj[row.key] = row.value;
-    }
-  });
-  return JSON.stringify(obj, null, 2);
-};
-
-// return json path in the format $["customer"]
-const jsonPathToString = (path?: NodeData["path"]) => {
-  if (!path || path.length === 0) return "$";
-  return `$["${path.join('"]["')}"]`;
+  return JSON.stringify(text, replacer, 2);
 };
 
 export const NodeModal = ({ opened, onClose }: ModalProps) => {
-  const nodeData = useGraph(state => state.selectedNode);
+  const nodeData = useGraph(state => dataToString(state.selectedNode?.text));
+  const path = useGraph(state => state.selectedNode?.path || "");
 
   return (
-    <Modal size="auto" opened={opened} onClose={onClose} centered withCloseButton={false}>
-      <Stack pb="sm" gap="sm">
+    <Modal title="Node Content" size="auto" opened={opened} onClose={onClose} centered>
+      <Stack py="sm" gap="sm">
         <Stack gap="xs">
-          <Flex justify="space-between" align="center">
-            <Text fz="xs" fw={500}>
-              Content
-            </Text>
-            <CloseButton onClick={onClose} />
-          </Flex>
+          <Text fz="xs" fw={500}>
+            Content
+          </Text>
           <ScrollArea.Autosize mah={250} maw={600}>
-            <CodeHighlight
-              code={normalizeNodeData(nodeData?.text ?? [])}
-              miw={350}
-              maw={600}
-              language="json"
-              withCopyButton
-            />
+            <CodeHighlight code={nodeData} miw={350} maw={600} language="json" withCopyButton />
           </ScrollArea.Autosize>
         </Stack>
         <Text fz="xs" fw={500}>
@@ -53,7 +34,7 @@ export const NodeModal = ({ opened, onClose }: ModalProps) => {
         </Text>
         <ScrollArea.Autosize maw={600}>
           <CodeHighlight
-            code={jsonPathToString(nodeData?.path)}
+            code={path}
             miw={350}
             mah={250}
             language="json"
