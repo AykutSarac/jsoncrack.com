d4adb12f1c4be276b5f6cc1631fe6f1900cf537b
JustinSui624 <jsui@ufl.edu>
2025-07-28 22:18:49 -0400

    ok

--- src/features/modals/NodeModal/index.tsx
+++ src/features/modals/NodeModal/index.tsx
@@ -53,6 +53,7 @@ export const NodeModal = ({ opened, onClose }: ModalProps) => {
           obj = obj[key];
         }
         obj[pathArr[pathArr.length - 1]] = newValue;
+        setContents({ contents: JSON.stringify(updatedJson, null, 2) });
       }
 
       setEditing(false);
