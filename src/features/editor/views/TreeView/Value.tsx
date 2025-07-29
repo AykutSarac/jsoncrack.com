fda2be5dffd875c4b4f1b6244c2efc94a85743de
JustinSui624 <jsui@ufl.edu>
2025-07-28 20:02:01 -0400

    first try

--- src/features/editor/views/TreeView/Value.tsx
+++ src/features/editor/views/TreeView/Value.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { DefaultTheme } from "styled-components";
 import { useTheme } from "styled-components";
 import { TextRenderer } from "../GraphView/CustomNode/TextRenderer";
+import { Popover, Button } from "@mantine/core";
 
 type TextColorFn = {
   theme: DefaultTheme;
@@ -26,17 +27,43 @@ interface ValueProps {
 export const Value = (props: ValueProps) => {
   const theme = useTheme();
   const { valueAsString, value } = props;
-
+  const [opened, setOpened] = React.useState(false);
+  
   return (
-    <span
-      style={{
-        color: getValueColor({
-          theme,
-          $value: valueAsString,
-        }),
-      }}
+    <Popover
+      opened={opened}
+      onChange={setOpened}
+      position="right"
+      withArrow
+      shadow="md"
+      withinPortal
     >
-      <TextRenderer>{JSON.stringify(value)}</TextRenderer>
-    </span>
+      <Popover.Target>
+        <span
+          style={{
+            color: getValueColor({
+              theme,
+              $value: valueAsString,
+            }),
+            cursor: "pointer",
+          }}
+          onClick={() => setOpened((o) => !o)}
+        >
+          <TextRenderer>{JSON.stringify(value)}</TextRenderer>
+        </span>
+      </Popover.Target>
+      <Popover.Dropdown>
+        <Button
+          size="xs"
+          onClick={() => {
+            setOpened(false);
+            // Add your edit logic here
+            alert("Edit clicked!");
+          }}
+        >
+          Edit
+        </Button>
+      </Popover.Dropdown>
+    </Popover>
   );
 };
