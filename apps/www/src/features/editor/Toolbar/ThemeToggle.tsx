ec9d208c5bc986a30d8d1e315bb4f2c5d150768a
smartcaveman <smartcaveman@gmail.com>
2026-03-25 15:55:50 -0400

    Update ThemeToggle title attribute to accurately reflect the purpose of the button

--- apps/www/src/features/editor/Toolbar/ThemeToggle.tsx
+++ apps/www/src/features/editor/Toolbar/ThemeToggle.tsx
@@ -7,7 +7,7 @@ export const ThemeToggle = () => {
   const toggleDarkMode = useConfig(state => state.toggleDarkMode);
 
   return (
-    <StyledToolElement title="Fullscreen" onClick={() => toggleDarkMode(!darkmodeEnabled)}>
+    <StyledToolElement title="{!darkmodeEnabled ? 'Dark' : 'Light'} Mode" onClick={() => toggleDarkMode(!darkmodeEnabled)}>
       {!darkmodeEnabled ? <FaMoon size="18" /> : <FaSun size="18" />}
     </StyledToolElement>
   );
